7a7369ea4cf4524e8285568878b196a1b55551ec
inkapmb <116178476+inkapmb@users.noreply.github.com>
2022-11-04 16:14:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 cd ./cmd
 
-for s in $(ls) do
-  sudo cp ./$s /usr/local/bin
-  sudo chmod a+rx /usr/local/bin/$s
+for s in $(ls) ; do
+	cp ./$s /usr/local/bin
+  	chmod a+rx /usr/local/bin/$s
 done
 
 echo Installed!
